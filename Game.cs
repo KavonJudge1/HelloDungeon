fe00c0449b9a0446ee676295362ce65785bea01b
KavonJudge1 <judgekavon1@gmail.com>
2024-08-28 10:47:53 -0500

    Initial Commit

---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Dungeon
+{
+    internal class Game
+    {
+        public void Run()
+        {
+            string playerName = "Boblius";
+            float playerHealth = 10.0f;
+            float playerMana = 5.0f;
+            int playerGold = 3;
+            
+
+            Console.WriteLine("Hello," + playerName);
+            Console.WriteLine();
+            Console.WriteLine("Welcome to my dungeon!");
+            Console.WriteLine();
+            Console.WriteLine("Health: " + playerHealth);
+            Console.WriteLine("Mana:" + playerMana);
+            Console.WriteLine)"Gold: " + playerGold);
+            Console.WriteLine();
+            Console.WriteLine("You find yourself in a generic dungeon with two doors. Which door do you choose?");
+            
+            
+        }
+    }
+}
