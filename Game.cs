3db47c6893652ba3b87ef8f42f0a09958524a24c
KavonJudge1 <judgekavon1@gmail.com>
2024-08-30 12:05:26 -0500

    Add files via upload

--- Game.cs
+++ Game.cs
@@ -14,7 +14,8 @@ namespace Hello_Dungeon
             float playerHealth = 10.0f;
             float playerMana = 5.0f;
             int playerGold = 3;
-            
+            string playerRole = "Warrior";
+            bool isDead = false;
 
             Console.WriteLine("Hello," + playerName);
             Console.WriteLine();
@@ -22,11 +23,71 @@ namespace Hello_Dungeon
             Console.WriteLine();
             Console.WriteLine("Health: " + playerHealth);
             Console.WriteLine("Mana:" + playerMana);
-            Console.WriteLine)"Gold: " + playerGold);
+            Console.WriteLine("Gold: " + playerGold);
             Console.WriteLine();
-            Console.WriteLine("You find yourself in a generic dungeon with two doors. Which door do you choose?");
-            
-            
-        }
-    }
-}
+            int input = GetInput("Are you a warrior or a wizard?", "Warrior", "Wizard");
+
+            string input = "";
+
+            while (input != "1" && input != "2");
+            {
+                Console.WriteLine("(1 | 2)");
+                input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    playerRole = "Warrior";
+                }
+                else if (input == "2")
+                {
+                    playerRole = "Wizard";
+                }
+
+
+                Console.WriteLine("Player role: " + playerRole);
+
+
+
+
+            } 
+
+            int GetInput(string description, string option1, string option2)
+            {
+                string input = "";
+                int inputRecieved = 0;
+
+                // Input loop
+                while (inputRecieved != 1 && inputRecieved != 2)
+                {
+                    // Print options
+                    Console.WriteLine(description);
+                    Console.WriteLine("1. " + option1);
+                    Console.WriteLine("2. " + option2);
+                    Console.Write("> ");
+
+                    // Get input from player
+                    input = Console.ReadLine();
+
+                    //if player selected the first option
+                    if (input == "1" || input == option1)
+                    {
+                        // Set inputRecieved to be the first option
+                        inputRecieved = 1;
+                    }
+                    // Otherwise if the player selected the second option
+                    else if (input == "2" || input == option2)
+                    {
+                        //Set inputRecieved to be the second option
+                        inputRecieved = 2;
+                    }
+                    // If neither are true
+                    else
+                    {
+                        //Display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey();
+                    }
+                }
+
+            }   
+
